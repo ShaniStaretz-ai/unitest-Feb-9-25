38ed3585e5b41add0eaf5827cf4880293f88e5c7
Shani Staretz <shakimi88@gmail.com>
2025-02-10 20:54:37 +0200

    complete hw

--- calculator.py
+++ calculator.py
@@ -1,7 +1,9 @@
 import math
 
+
 def add(a, b):
-    return a + b #+ 0.1 # 0.99999999999
+    return a + b  # + 0.1 # 0.99999999999
+
 
 # 0.4444444444 + 0.66666666666
 # 0.999999999999
@@ -10,8 +12,35 @@ def add(a, b):
 def minus(a, b):
     return a - b
 
+
 def multiply(a, b):
     return a * b
 
+
 def divide(a, b):
     return a / b
+
+def say_hello():
+    name = input("enter name? ")
+    return f"hello {name}"
+
+#########################################################
+#homework
+
+def power(a: int, b: int) -> int:
+    return a ** b;
+
+
+def sqrt(a: int) -> float:
+    return math.sqrt(a);
+
+
+def factorial(a) -> int:
+    result = 1
+    if a < 0:
+        raise ValueError();
+    if a==0:
+        return 1
+    for i in range(1, a + 1):
+        result *= i
+    return result
--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,7 @@
-import time
-
 import pytest
+
 import calculator
+
 
 def test_calculator_add_small():
     # Arrange
@@ -15,6 +15,7 @@ def test_calculator_add_small():
     # Assert
     assert expected == actual, "small numbers add"
 
+
 def test_calculator_minus_small():
     # Arrange
     a: int = 5
@@ -26,6 +27,7 @@ def test_calculator_minus_small():
 
     # Assert
     assert expected == actual, "small numbers minus"
+
 
 def test_calculator_multiply_small():
     # Arrange
@@ -39,21 +41,69 @@ def test_calculator_multiply_small():
     # Assert
     assert expected == actual, "small numbers multiply"
 
+
 def test_calculator_divide_small():
     # Arrange
     a: int = 10
     b: int = 5
-    expected: int = 3
+    expected: int = 2
 
     # Act
     actual: int = calculator.divide(a, b)
 
     # Assert
     assert expected == actual, "small numbers divide"
-# pip install pytest
-# option 1- run the tests : play
-# option 2- run the tests
-#         in the Terminal : pytest .
-# add test sub
-# add test mul
-# add test div
+
+
+def test_calculator_hello(monkeypatch):
+    # black box
+    monkeypatch.setattr('builtins.input', lambda _: "danny")
+
+    expected = "hello danny"
+    result = calculator.say_hello()
+
+    assert expected == result
+
+
+########################################################
+
+@pytest.mark.parametrize("a,b,expected", [(2, 4, 16), (3, 2, 9)])  # Arrange
+def test_calculator_power_small(a, b, expected):
+    # Act
+    actual: int = calculator.power(a, b)
+
+    # Assert
+    assert expected == actual, f"sent {a},{b} ,expected {expected} :power small numbers"
+
+
+@pytest.mark.parametrize("a,expected", [(25, 5)])  # Arrange
+def test_calculator_sqrt_small_positive(a, expected):
+    # Act
+    actual: float = calculator.sqrt(a)
+
+    # Assert
+    assert expected == actual, f"sent {a},expected {expected} : sqrt small positive numbers  "
+
+
+def test_calculator_sqrt_small_negative():
+    # Arrange
+    a: int = -5
+    # Act
+    with pytest.raises(ValueError) as ex:
+        actual: float = calculator.sqrt(a)
+
+
+@pytest.mark.parametrize("a,expected", [(4, 24), (5, 120),(0,1)])
+def test_calculator_factorial_small_positive_numbers(a, expected):
+    # Act
+    actual: float = calculator.factorial(a)
+    assert expected == actual, f"sent {a},expected {expected} : factorial small positive numbers  "
+
+
+def test_calculator_factorial_small_negative_numbers():
+    # Arrange
+    a: int = -3
+    # Act
+    with pytest.raises(ValueError) as ex:
+        actual: float = calculator.factorial(a)
+
